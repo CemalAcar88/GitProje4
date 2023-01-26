7e09e7872fa28c2fac829967eebb2c47293c84a0
CemalAcar88 <kkt.cemal@gmail.com>
2023-01-26 19:09:57 +0200

    task 6 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,5 +4,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("tekrarmerhaba");
+        System.out.println("merhaba 3");
     }
 }
